07a9abb242c286ea78c6d5805dc68c61a0d0a6a0
Guilherme Almada <guido.ptr13@gmail.com>
2022-05-20 19:45:28 -0300

    feat(base/select): created base select component

---
+++ src/components/Base/BaseSelect.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <label for="base-select">
+            {{ label }}
+        </label>
+
+        <b-form-select
+        v-model="preSelected"
+          :options="options"
+          @input="inputHandler"
+        />
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseSelect',
+
+  props: {
+    label: {
+      type: String,
+      default: '',
+      required: false
+    },
+
+    options: {
+      type: Array,
+      required: true
+    },
+
+    preSelected: {
+      type: String,
+      required: false,
+      default: ''
+    }
+  },
+
+  methods: {
+    inputHandler (value) {
+      this.$emit('input', value)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
